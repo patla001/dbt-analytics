760b1fc635ffc063c313f210e528db5bac895100
Ezer Patlan <epatlan1742@sdsu.edu>
2025-10-16 03:37:32 +0000

    test

--- models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
@@ -1,6 +1,29 @@
-select
-    id as order_id,
-    user_id as customer_id,
-    order_date,
-    status
-from {{ source('jaffle_shop', 'orders') }}
+-- select
+--     id as order_id,
+--     user_id as customer_id,
+--     order_date,
+--     status
+-- from {{ source('jaffle_shop', 'orders') }}
+
+with 
+
+source as (
+
+    select * from {{ source ('jaffle_shop', 'orders')}}
+
+),
+
+renamed as (
+
+    select
+        id as order_id,
+        user_id as customer_id,
+        order_date,
+        status as order_status,
+        current_timestamp() as etl_loaded_at
+        
+
+from source
+)
+
+select * from renamed
