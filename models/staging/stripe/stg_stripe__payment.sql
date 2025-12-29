10e01b54b296521ec6d6d0b67ffea54fa7534629
Ezer Patlan <epatlan1742@sdsu.edu>
2025-12-29 16:20:40 +0000

    update dbt test

--- models/staging/stripe/stg_stripe__payment.sql
+++ models/staging/stripe/stg_stripe__payment.sql
@@ -1,11 +1,32 @@
-select 
-    id as payment_id,
-    orderid as order_id,
-    paymentmethod as payment_method,
-    status,
+with
+source as (
+    select * from {{ source('stripe','payment')}}
 
-    -- amount is stored in cents, convert it to dollars
-    amount / 100 as amount,
-    created as created_at
+),
+
+renamed as (
+        select
+            id as payment_id,
+            orderid as order_id,
+            paymentmethod as payment_method,
+            status as payment_status,
+            amount,
+            created
+            -- _batched_at
+        from source
+
+)
+
+select * from renamed
+
+-- select 
+--     id as payment_id,
+--     orderid as order_id,
+--     paymentmethod as payment_method,
+--     status,
+
+--     -- amount is stored in cents, convert it to dollars
+--     amount / 100 as amount,
+--     created as created_at
     
- from {{ source('stripe', 'payment') }}
+--  from {{ source('stripe', 'payment') }}
