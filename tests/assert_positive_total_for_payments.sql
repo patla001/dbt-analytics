bb393d1dde142e26e6c201184d65bcd399283e13
Ezer Patlan <epatlan1742@sdsu.edu>
2025-12-29 23:53:53 +0000

    added single test payment

--- tests/assert_positive_total_for_payments.sql
+++ tests/assert_positive_total_for_payments.sql
@@ -1,6 +1,8 @@
 select
-    orderid,
+    --orderid,
+    order_id,
     sum(amount) as total_amount
-from {{source('stripe','payment')}}
+--from {{source('stripe','payment')}}
+from {{ ref('stg_stripe_payment') }}
 group by 1
 having sum(amount) < 0
